dba6b96fb6ccd3666f8e5b47f168aab5891a9f5b
Bryan Goss <bcgoss@gmail.com>
2016-08-10 13:37:16 -0600

    I4: Cheat Mode, add &cheat=true to url

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,19 +5,36 @@ SECRET_NUMBER = Random.rand(100) + 1
 
 get '/' do
   guess = params['guess'].to_i
-  message = check_guess(guess)
+  cheat = params['cheat']
+  message = check_guess(guess, cheat)
   erb :index, :locals => {number: SECRET_NUMBER, message: message}
 end
 
-def check_guess(guess)
-  if guess == 0
-    return "Guess a number between 1 and 100"
-  end
-  if guess > SECRET_NUMBER
-    message = "Too High!"
-  elsif guess < SECRET_NUMBER
-    message = "Too Low!"
+def check_guess(guess, cheat)
+  cheat_message(cheat) + answer(guess)
+end
+
+def cheat_message(cheat)
+  if cheat
+    "The secret number is #{SECRET_NUMBER}, AND YOU CHEATED!</p><p>"
   else
-    message = "You got it right!"
+    ""
   end
+end
+
+def answer(guess)
+  return "Guess a number between 1 and 100" if guess == 0
+  return "Way too high!" if way_too_high?(guess)
+  return "Way too low!" if way_too_low?(guess)
+  return "Too high!" if guess > SECRET_NUMBER
+  return "Too low!" if guess < SECRET_NUMBER
+  "You got it right!</p><p>The secret number is #{SECRET_NUMBER}"
+end
+
+def way_too_high?(guess)
+  guess > SECRET_NUMBER and guess - 5 > SECRET_NUMBER
+end
+
+def way_too_low?(guess)
+  guess < SECRET_NUMBER and guess + 5 < SECRET_NUMBER
 end
